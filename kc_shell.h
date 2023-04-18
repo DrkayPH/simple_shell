264a04ce8f525bb0bc16df9795e9a5b3952501bc
DrkayPH <oghenekarouche@gmail.com>
2023-04-18 03:00:11 -0700

    prototype functions

---
+++ kc_shell.h
@@ -0,0 +1,21 @@
+/** List of function prototypes for kc_shell by:
+ * @DrKayPH
+ * @Clement-tech
+ * for ALX-SE simple_shell cohort 12 task
+ */
+#ifndef KC_SHELL_H
+#define KC_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#endif
